d6001fc69da98e30a17aa834d7b9879ff923d3ed
Kate <cho_yj1023@naver.com>
2026-04-25 00:46:39 +0900

    [Bronze I] Title: 최소공배수, Time: 264 ms, Memory: 18504 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -3,48 +3,20 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int cnt;
-		int a, b;
-		int max=0;
-		
-		cnt = sc.nextInt();
-		
+		int cnt = sc.nextInt();
 		for(int i=0; i<cnt; ++i) {
-			a=sc.nextInt();
-			b=sc.nextInt();
-			if(a>b) {
-				for(int j=1; j<=a; ++j) {
-					if(a%j == 0 && b%j == 0) {
-						max = j;
-					}
-					else {
-						continue;
-					}
-				}
-				System.out.println((a*b)/max);
-			}
-			else if (a<b) {
-				for(int j=1; j<=b; ++j) {
-					if(a%j == 0 && b%j == 0) {
-						max = j;
-					}
-					else {
-						continue;
-					}
-				}
-				System.out.println((a*b)/max);
-			}
-			else {
-				for(int j=1; j<=b; ++j) {
-					if(a%j == 0 && b%j == 0) {
-						max = j;
-					}
-					else {
-						continue;
-					}
-				}
-				System.out.println((a*b)/max);
-			}
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int res = a*b/gcd(a, b);
+			System.out.println(res);
+		}
+	}
+	public static int gcd(int a, int b) {
+		if(b == 0) {
+			return a;
+		}
+		else {
+			return gcd(b, a%b);
 		}
 	}
 }
