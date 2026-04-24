c0f9f9ed323f10130ed3af5defd0bba55e181621
Kate <cho_yj1023@naver.com>
2026-04-25 00:43:32 +0900

    [Silver V] Title: 소트인사이드, Time: 108 ms, Memory: 12772 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,21 +1,26 @@
-//정렬
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        char[] c = new char[str.length()];
-        for(int i=0; i<c.length;++i) {
-        	c[i] = str.charAt(i);
-        }
-        Arrays.sort(c);
-        for(int i=c.length-1; i>=0; --i) {
-        	System.out.print(c[i]);
-        }
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int[] num = new int[str.length()];
+		for(int i=0; i<str.length(); ++i) {
+			num[i] = Integer.parseInt(str.substring(i, i+1));
+		}
+		for(int i=0; i<str.length(); ++i) {
+			int max = i;
+			for(int j = i+1; j<str.length(); ++j) {
+				if(num[j] > num[max]) max = j;
+			}
+			if(num[i] < num[max]) {
+				int tmp = num[i];
+				num[i] = num[max];
+				num[max]  = tmp;
+			}
+		}
+		for(int i=0; i<str.length(); ++i) {
+			System.out.print(num[i]);
+		}
+	}
 }
